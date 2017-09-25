fa2acc66707ed8e3b22bdc3cf85715b9d2527b19
Daniel Crawford <daniel.j.crawford@outlook.com>
2017-09-25 00:33:18 -0400

    Added Unit Testing for the Robots dll. Testing currently only covers factory methods and constructors for each RobotType and PartType

---
+++ UnitTest_Robots/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle ( "UnitTest_Robots" )]
+[assembly: AssemblyDescription ( "" )]
+[assembly: AssemblyConfiguration ( "" )]
+[assembly: AssemblyCompany ( "" )]
+[assembly: AssemblyProduct ( "UnitTest_Robots" )]
+[assembly: AssemblyCopyright ( "Copyright ©  2017" )]
+[assembly: AssemblyTrademark ( "" )]
+[assembly: AssemblyCulture ( "" )]
+
+[assembly: ComVisible ( false )]
+
+[assembly: Guid ( "b7c9940b-2281-40d6-a0c7-42f406cb5ae6" )]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion ( "1.0.0.0" )]
+[assembly: AssemblyFileVersion ( "1.0.0.0" )]
---
+++ UnitTest_Robots/UnitTest_Robots.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Robots;
+using Robots.RobotParts.Weapon;
+using Robots.RobotParts.Chassis;
+using Robots.RobotParts.Mobility;
+
+namespace UnitTest_Robots
+{
+    [TestClass]
+    public class UnitTest_Robots
+    {
+        [TestMethod]
+        public void ConstructAllRobotTypes ( )
+        {
+            foreach ( RobotTypes type in Enum.GetValues(typeof(RobotTypes)) ) 
+                Factory_Robot.Build ( type );
+            
+        }
+
+        [TestMethod]
+        public void ConstructAllWeaponTypes ( )
+        {
+            foreach ( WeaponPartTypes type in Enum.GetValues ( typeof ( WeaponPartTypes ) ) )
+                Factory_WeaponPart.Build ( type );
+        }
+
+        [TestMethod]
+        public void ConstructAllChassisTypes ( )
+        {
+            foreach ( ChassisPartTypes type in Enum.GetValues ( typeof ( ChassisPartTypes ) ) )
+                Factory_ChasisPart.Build ( type );
+        }
+
+        [TestMethod]
+        public void ConstructAllMobilityPartTypes ( )
+        {
+            foreach ( MobilityPartTypes type in Enum.GetValues ( typeof ( MobilityPartTypes ) ) )
+                Factory_MobilityPart.Build ( type );
+        }
+    }
+}
